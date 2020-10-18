4085a245afa48b2767081a569439568906239001
nailifd <73066418+nailifd@users.noreply.github.com>
2020-10-18 23:18:37 +0700

    Add files via upload

---
+++ Tugas1_123200095.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	string nama;
+	int nim;
+	cout<<"DATA PRIBADI\n";
+
+	cout<<"\nNama : ";
+	cin>>nama;
+	cout<<"NIM : ";
+	cin>>nim;
+
+
+	return 0;
+
+	
+}
